d9b4180b4d04116ee2fa1300af3c141398b70708
Rajendra Sarpal <sarpal465@gmail.com>
2020-08-24 08:38:23 +0530

    updated

--- actions.py
+++ actions.py
@@ -8,10 +8,11 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-
+from rasa.nlu.config import RasaNLUModelConfig
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
+import json
 
 #
 #
@@ -35,10 +36,18 @@ class ActionRespondCoroanStateCity(Action):
      def run(self, dispatcher, tracker, domain):
       # last_message = tracker.latest_message.get("text", "")
 
-      response = requests.get("https://api.covid19india.org/data.json").json()
-      print("Length ", len(response["statewise"]))
-      message = "Please enter correct STATE name"
-      
+
+      def api_call(country_name):
+          url = "https://covid-19-tracking.p.rapidapi.com/v1/" + country_name
+          #print(url)
+
+          headers = {'x-rapidapi-host': "covid-19-tracking.p.rapidapi.com",
+                     'x-rapidapi-key': "f2eed9bfd0msh2e7b8e60c2912d1p126f44jsn176f4a33b42d"}
+
+          response = requests.request("GET", url, headers=headers)
+          responsee = json.loads(response.text)
+          return(responsee)
+     
 
       entities = tracker.latest_message['entities']
       print("Last Message Now ", entities)
@@ -47,22 +56,15 @@ class ActionRespondCoroanStateCity(Action):
             if e['entity'] == "state":
                 state = e['value']
       
-      print("State ", state) 
-      #state = state.lower()     
-      print("State ", state) 
-      if state == "corona":
-          state = "Total"
-      if state == "india":
-          state = "Total"   
+     
 
-      # print("State ", state.title())   
-      message = "Please enter correct STATE name"
+      print("State ", state.title())   
+      message = "Please say correct Country name"
       if(state != None):
-          message = "Please enter correct STATE name"
-          for data in response["statewise"]:
-              if data["state"] == state.title():
-                  print(data)
-                  message = "Active: "+data["active"] +" Confirmed: " + data["confirmed"] +" Recovered: " + data["recovered"] +" On "+data["lastupdatedtime"]
+          message = "Please say correct Country name"
+          data = api_call(state.title())
+          print(data)
+          message = "Active: "+data["Active Cases_text"] +" Confirmed: " + data["Total Cases_text"] +" Recovered: " + data["Total Recovered_text"] + "Death: " + data["Total Deaths_text"] + " On "+data["Last Update"]
 
       dispatcher.utter_message(message)
 
