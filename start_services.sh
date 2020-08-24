3c4c3c5b82d163a8cf9f5ae7fbb790b6e3b06308
manoj1440 <30985016+manoj1440@users.noreply.github.com>
2020-08-24 12:19:10 +0530

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,1 @@
-cd app/
-# Start rasa server with nlu model
-
-rasa run --model models --enable-api --cors "*" --debug \
-         -p $PORT
-         
-rasa run actions         
+rasa run --model models --enable-api --cors "*" --debug -p $PORT & rasa run actions         
